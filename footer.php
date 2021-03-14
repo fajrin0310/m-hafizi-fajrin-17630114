fd893460e45b13c5284f60408d54bb14788267a8
fajrin0310 <fajrin0310@gmail.com>
2021-03-14 14:57:14 +0700

    comit pertama dari projek pratikum M. Hafizi Fajrin NPM:17630114

---
+++ footer.php
@@ -0,0 +1,14 @@
+</div>
+
+  <footer>
+    <div class="container">
+      <span>&copy; 2020 - FTI UNISKA</span>
+    </div>
+  </footer>
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+  </html>
---
+++ profile.php
@@ -0,0 +1,49 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/fajrin.jpg" alt="M. Hafizi fajrin" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NIDN :</strong></td>
+                        <td align="left" width="50%">620301121299006 / 1131129902</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">M.Hafizi Fajrin</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Kapuas, 12 Desember 1999</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">082256338691</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">fajrin0310@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/mhafizifajrin" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/036-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/fajrin_1212" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/mhafizifajrin" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/008-twitter.svg" width="30" alt="twitter">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
